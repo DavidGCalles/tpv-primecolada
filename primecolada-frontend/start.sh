65169342318badcd18fafd9694e68da10a26848e
DavidGCalles <davidgcalles91@gmail.com>
2025-10-30 17:23:57 +0100

    Reintegrating backend to deployment

--- primecolada-frontend/start.sh
+++ primecolada-frontend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set default values for environment variables if they are not already set
-export BACKEND_HOST=${BACKEND_HOST:-backend}
+export BACKEND_HOST=${BACKEND_HOST:-localhost}
 export BACKEND_PORT=${BACKEND_PORT:-5000}
 
 # Substitute environment variables in the nginx template
