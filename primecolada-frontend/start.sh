8a1371cd33a23b68bcfc4fba0414958aaffa5efd
DavidGCalles <davidgcalles91@gmail.com>
2025-10-31 09:28:05 +0100

    nginx proxy to proper backend

--- primecolada-frontend/start.sh
+++ primecolada-frontend/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# Set default values for environment variables if they are not already set
-export BACKEND_HOST=${BACKEND_HOST:-localhost}
-export BACKEND_PORT=${BACKEND_PORT:-5000}
+# If BACKEND_URL is set, use it. Otherwise, construct from host and port.
+if [ -n "$BACKEND_URL" ]; then
+  export PROXY_PASS_URL=$BACKEND_URL
+else
+  export PROXY_PASS_URL="http://${BACKEND_HOST:-localhost}:${BACKEND_PORT:-5000}"
+fi
 
 # Substitute environment variables in the nginx template
-envsubst '${PORT} ${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${PROXY_PASS_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
